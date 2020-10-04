35c233ff5c1191fee00db2304fb69849dc9d8a00
maryk <4nofate@gmail.com>
2020-10-03 23:33:34 -0700

    https://www.codewars.com/users/star-algol/completed_solutions

---
+++ Simple Fun #152: Invite More Women?.js
@@ -0,0 +1,7 @@
+function inviteMoreWomen(L) {
+    let sum = 0;
+    for(let i = 0; i< L.length; i++){
+        sum = sum + L[i];
+    }
+    return sum>0;
+}//https://www.codewars.com/users/star-algol/completed_solutions
