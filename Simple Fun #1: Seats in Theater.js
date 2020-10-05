9faa498b7abce532ec5849a9cead448f12f7b43d
maryk <4nofate@gmail.com>
2020-10-04 23:33:35 -0700

    https://www.codewars.com/kata/588417e576933b0ec9000045

---
+++ Simple Fun #1: Seats in Theater.js
@@ -0,0 +1,4 @@
+function seatsInTheater(nCols, nRows, col, row) {
+    let disturbedPpl = (nCols-col+1)*(nRows-row);
+    return disturbedPpl;
+}//https://www.codewars.com/kata/588417e576933b0ec9000045
