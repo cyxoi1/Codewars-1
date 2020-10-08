adbc5f87ff798b905de1e435a779eecea97679f2
maryk <4nofate@gmail.com>
2020-10-07 23:40:24 -0700

    https://www.codewars.com/kata/5b0d67c1cb35dfa10b0022c7

---
+++ The wheat/rice and chessboard problem_2 variant.js
@@ -0,0 +1,9 @@
+function squaresNeeded(grains){
+    let sum = 0;
+    let a = 0;
+    while (sum < grains){
+        sum+=2**a;
+        a++;
+
+    }return a;
+}//https://www.codewars.com/kata/5b0d67c1cb35dfa10b0022c7
