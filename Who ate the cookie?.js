db491f88906be6a4daa1b787d152357ea337b3a5
maryk <4nofate@gmail.com>
2020-10-08 23:13:25 -0700

    https://www.codewars.com/kata/55a996e0e8520afab9000055

---
+++ Who ate the cookie?.js
@@ -0,0 +1,12 @@
+function cookie(x){
+    let name;
+    if (typeof x=== 'string'){
+        name = "Zach";
+    } else if(typeof x=== 'number'){
+        name = "Monica";
+    } else {
+        name = "the dog";
+    }
+    return `Who ate the last cookie? It was ${name}!`
+}
+//https://www.codewars.com/kata/55a996e0e8520afab9000055
