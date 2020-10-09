dbd307f7f682771c24d4da3d051c912cacdb337d
maryk <4nofate@gmail.com>
2020-10-08 23:12:23 -0700

    https://www.codewars.com/kata/55a5bfaa756cfede78000026

--- Convert boolean values to strings 'Yes' or 'No'..js
+++ Convert boolean values to strings 'Yes' or 'No'..js
@@ -1,4 +1,4 @@
 function boolToWord( bool ){
     return bool===true?'Yes':'No';
 }
-//https://www.codewars.com/kata/53369039d7ab3ac506000467
+// https://www.codewars.com/kata/53369039d7ab3ac506000467
---
+++ Super Duper Easy.js
@@ -0,0 +1,3 @@
+function problem(x){
+    return (typeof x==='number')?(x*50+6):'Error';
+}//https://www.codewars.com/kata/55a5bfaa756cfede78000026
