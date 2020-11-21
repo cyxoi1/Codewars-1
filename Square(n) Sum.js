1e37a964f48b68db8180367514cb968f8303807c
star-algol <4nofate@gmail.com>
2020-11-21 12:09:44 -0800

    https://www.codewars.com/kata/515e271a311df0350d00000f/train/javascript

---
+++ Square(n) Sum.js
@@ -0,0 +1,8 @@
+function squareSum(numbers){
+    let sum = 0;
+    for(let i = 0; i<numbers.length; i++){
+        sum = sum + numbers[i]**2;
+    }
+    return sum;
+}
+//https://www.codewars.com/kata/515e271a311df0350d00000f/train/javascript
